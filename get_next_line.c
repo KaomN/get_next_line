c66357275306fd1bc8521bdbce4dbc64ea02fdcd
Cong Nguyen <kaom.n.92@gmail.com>
2021-12-28 14:51:09 +0200

    Added check for newline before reading and joining together the strings

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,92 @@
 /*   By: conguyen <conguyen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 17:05:21 by conguyen          #+#    #+#             */
-/*   Updated: 2021/12/21 14:43:25 by conguyen         ###   ########.fr       */
+/*   Updated: 2021/12/28 14:47:35 by conguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_get_line(char **saved, char **line)
+static int	ft_get_newline(char **saved, int c, char **line)
+{
+	char	*temp;
+
+	*line = ft_strsub(*saved, 0, c);
+	if (*line == NULL)
+	{
+		free(*saved);
+		return (-1);
+	}
+	temp = ft_strdup(&((*saved)[c + 1]));
+	if (temp == NULL)
+	{
+		free(*saved);
+		return (-1);
+	}
+	free(*saved);
+	*saved = temp;
+	if ((**saved) == '\0')
+		ft_strdel(saved);
+	return (1);
+}
+
+static int	ft_get_endofline(char **saved, char **line)
 {
 	int		c;
-	char	*temp;
 
 	c = 0;
 	while ((*saved)[c] != '\0' && (*saved)[c] != '\n')
 		c++;
 	if ((*saved)[c] == '\n')
-	{
-		*line = ft_strsub(*saved, 0, c);
-		temp = ft_strdup(&((*saved)[c + 1]));
-		if (temp == NULL)
-			return (-1);
-		free(*saved);
-		*saved = temp;
-		if ((**saved) == '\0')
-			ft_strdel(saved);
-	}
+		return (ft_get_newline(saved, c, line));
 	else
 	{
 		*line = ft_strdup(*saved);
+		if (*line == NULL)
+		{
+			ft_strdel(saved);
+			return (-1);
+		}
 		ft_strdel(saved);
 	}
 	return (1);
 }
 
-static int	ft_save_buffer(char *buf, char **line, char **saved, int fd)
+static int	ft_save_buffer(char **saved, char *buf)
 {
-	char		*temp;
+	char	*temp;
+
+	if (*saved != NULL)
+	{
+		temp = ft_strjoin(*saved, buf);
+		if (temp == NULL)
+		{
+			free(*saved);
+			return (-1);
+		}
+		free(*saved);
+		*saved = temp;
+	}
+	else
+	{
+		*saved = ft_strdup(buf);
+		if (*saved == NULL)
+			return (-1);
+	}
+	return (1);
+}
+
+int	get_next_line(const int fd, char **line)
+{
+	char		buf[BUFF_SIZE + 1];
+	static char	*saved[MAX_FD];
 	int			ret;
 
+	if (fd < 0 || fd >= 4096 || line == NULL || BUFF_SIZE <= 0)
+		return (-1);
+	if (saved[fd] != NULL)
+		if (ft_strchr(saved[fd], '\n') != NULL)
+			return (ft_get_endofline(&saved[fd], line));
 	ret = 1;
 	while (ret)
 	{
@@ -53,29 +101,11 @@ static int	ft_save_buffer(char *buf, char **line, char **saved, int fd)
 		if (ret == 0 && saved[fd] == NULL)
 			return (0);
 		buf[ret] = '\0';
-		if (saved[fd] != NULL)
-		{
-			temp = ft_strjoin(saved[fd], buf);
-			free(saved[fd]);
-			saved[fd] = temp;
-		}
+		if (ft_save_buffer(&saved[fd], buf) == -1)
+			return (-1);
 		else
-			saved[fd] = ft_strdup(buf);
-		if (ft_strchr(saved[fd], '\n') != NULL)
-			break ;
+			if (saved[fd] != NULL && ft_strchr(saved[fd], '\n') != NULL)
+				break ;
 	}
-	return (ft_get_line(&saved[fd], line));
-}
-
-int	get_next_line(const int fd, char **line)
-{
-	char		buf[BUFF_SIZE + 1];
-	static char	*saved[MAX_FD];
-
-	if (fd < 0 || line == NULL || BUFF_SIZE <= 0)
-		return (-1);
-	if (saved[fd] != NULL)
-		if (ft_strchr(saved[fd], '\n') != NULL)
-			return (ft_get_line(&saved[fd], line));
-	return (ft_save_buffer(buf, line, &saved[fd], fd));
+	return (ft_get_endofline(&saved[fd], line));
 }
